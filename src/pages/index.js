a38a409507ed1a40d0abb9847d86cfa47cedfdd9
Jay <jay.bienaime@gmail.com>
2020-05-20 07:43:20 -0700

    PC_BROKEN
    SWITCHING PCS

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const colors = ["#7ED56F", "55C57A", "28B485"];
+
+
+export default () => (<div>
+<header className={"header"}>
+        <div className="header-content">
+        <h1 className="heading-primary text__light" >
+            <span className="heading-primary__main">Dione</span>
+            <span className="heading-primary__sub">Unimaginative names based on gods</span>
+        </h1>
+        <div className="action">
+            <a href="#" role="button" className="btn btn__white">Visit</a>
+        </div>
+        </div>
+
+
+</header>
+
+</div>)
